989f494abddb2f187f5122ddb2c0cbd3790e6476
t <topeklc@gmail.com>
2022-03-05 20:09:04 +0100

    commit to first deploy

--- scrapers/updater.py
+++ scrapers/updater.py
@@ -4,5 +4,5 @@ from scrapers import news_scrapers
 
 def start():
     scheduler = BackgroundScheduler()
-    scheduler.add_job(news_scrapers.save_to_db, "interval", minutes=180)
+    scheduler.add_job(news_scrapers.save_to_db, "interval", minutes=5)
     scheduler.start()
